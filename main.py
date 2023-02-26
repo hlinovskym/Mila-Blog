05b113e804e217fd61533467ed3327f53270fa56
Mila <hlinovskym@seznam.cz>
2023-02-26 09:25:23 +0100

    add db

--- main.py
+++ main.py
@@ -34,6 +34,7 @@ Bootstrap(app)
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 # remote db - PostgreSQL
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL",  "sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
